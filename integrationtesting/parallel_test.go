54ac9b314509fbafc7f512163802c0af6c293bbe
fireagainsmile <952577382@qq.com>
2019-09-20 09:14:34 +0800

    add test cases

--- integrationtesting/parallel_test.go
+++ integrationtesting/parallel_test.go
@@ -2,6 +2,7 @@ package tester
 
 import (
     "fmt"
+    "math/big"
     "os"
     "sync"
     "testing"
@@ -19,13 +20,16 @@ type account struct {
 
 var (
     ipPort       = "26657"                     // ip port of nodes
-    node1        = os.Getenv("ANKR_URL")
+    //node1        = os.Getenv("ANKR_URL")
+    node1 = "chain-dev.dccn.ankr.com"
     node2        = os.Getenv("ANKR_URL")
     adminAddress = "B508ED0D54597D516A680E7951F18CAD24C7EC9FCFCD67"
     adminPrivKey = "wmyZZoMedWlsPUDVCOy+TiVcrIBPcn3WJN8k5cPQgIvC8cbcR10FtdAdzIlqXQJL9hBw1i0RsVjF6Oep/06Ezg==" //node amin private key
-    sendVal      = "20000000000000000000"
-    sendVal2     = "10000000000000000000"
+    sendVal      = "20000000000000000000" //20 ankr token
+    sendVal2     = "10000000000000000000" // 10 ankr token
     expectVal2   = "15000000000000000000"
+    leftVal = "5000000000000000000"
+    balBase = "1000000000000000000"
 )
 
 func checkoutTestAccount() {
@@ -34,107 +38,164 @@ func checkoutTestAccount() {
         return
     }
 }
+
+// send coins, check single transaction logic
+func TestSendSingleTransaction(t *testing.T)  {
+    base, _ := new(big.Int).SetString(balBase, 10)
+    initPriv, _, initAddr := wallet.GenerateKeys()
+    initAmount := new(big.Int)
+    initAmount = initAmount.Mul(base, big.NewInt(20))
+    _, err := wallet.SendCoins(node1, ipPort, adminPrivKey, adminAddress, initAddr, initAmount.String())
+    if err != nil {
+        t.Error(err)
+    }
+    receivePriv, _, receiveAddr := wallet.GenerateKeys()
+    sendAmount := new(big.Int)
+    leftAmount := initAmount.Mul(base, big.NewInt(15))
+
+    Convey("test send more than account balance", t, func() {
+      sendAmount = sendAmount.Mul(base, big.NewInt(30))
+      _, err = wallet.SendCoins(node1, ipPort, initPriv, initAddr, receiveAddr, sendAmount.String())
+      So(err, ShouldBeError)
+      bal, err := wallet.GetBalance(node1, ipPort, initAddr)
+      So(err, ShouldBeNil)
+      So(bal, ShouldEqual, leftAmount.String())
+      _, err = wallet.GetBalance(node1, ipPort, receiveAddr)
+      So(err, ShouldBeError)
+    })
+
+    Convey("test send all the amount of the account", t, func() {
+        sendAmount = sendAmount.Mul(base, big.NewInt(15))
+        _, err = wallet.SendCoins(node1, ipPort, initPriv, initAddr, receiveAddr, sendAmount.String())
+        So(err, ShouldBeNil)
+        bal, err := wallet.GetBalance(node1, ipPort, initAddr)
+        So(err, ShouldBeNil)
+        So(bal, ShouldEqual, "0")
+        bal, err = wallet.GetBalance(node1, ipPort, receiveAddr)
+        So(err, ShouldBeNil)
+        leftAmount = leftAmount.Mul(base, big.NewInt(10))
+        So(bal, ShouldEqual, leftAmount.String())
+    })
+
+    Convey("test send back", t, func() {
+        sendAmount = sendAmount.Mul(base, big.NewInt(10))
+        _, err = wallet.SendCoins(node1, ipPort, receivePriv, receiveAddr, initAddr, sendAmount.String())
+        So(err, ShouldBeNil)
+        bal, err := wallet.GetBalance(node1, ipPort, receiveAddr)
+        So(err, ShouldBeNil)
+        So(bal, ShouldEqual, "0")
+        bal, err = wallet.GetBalance(node1, ipPort, initAddr)
+        So(err, ShouldBeNil)
+        leftAmount = leftAmount.Mul(base, big.NewInt(5))
+        So(bal, ShouldEqual, leftAmount.String())
+    })
+}
+
+// test multiple account send to one account at the same time
 func TestMultSendOne(t *testing.T) {
     checkoutTestAccount()
     var accounts []account
+    gasAccount := "64BC85F08C03F42B17EAAF5AFFAF9BFAF96CFCB85CA2F3"
+    gasBal, _ := wallet.GetBalance(node1, ipPort, gasAccount)
+    t.Log(gasAccount,gasBal)
     for i := 0; i < 6; i++ {
         priv, pub, addr := wallet.GenerateKeys()
         _, err := wallet.SendCoins(node1, ipPort, adminPrivKey, adminAddress, addr, sendVal)
         ShouldBeNil(err)
         accounts = append(accounts, account{priv: priv, pub: pub, addr: addr})
-        bal, err := wallet.GetBalance(node1, ipPort, accounts[0].addr)
+        bal, err := wallet.GetBalance(node1, ipPort, addr)
         ShouldBeNil(err)
-        ShouldNotEqual(bal, expectVal2)
+        ShouldEqual(bal, expectVal2)
     }
     var wg sync.WaitGroup
-    for j := 1; j < 6; j++ {
+    _, _, receiveAddr := wallet.GenerateKeys()
+    for j := 0; j < 6; j++ {
         wg.Add(1)
-        go func(j int, wgg *sync.WaitGroup) {
-            _, err := wallet.SendCoins(node1, ipPort, accounts[j].priv, accounts[j].addr, accounts[0].addr, sendVal2)
+        go func(j int) {
+            _, err := wallet.SendCoins(node1, ipPort, accounts[j].priv, accounts[j].addr, receiveAddr, sendVal2)
             if err != nil {
                 t.Errorf("TestMultSendOne send error %s", err)
             }
-            wgg.Done()
-        }(j, &wg)
+            bal, err := wallet.GetBalance(node1, ipPort, accounts[j].addr)
+            if err != nil {
+                t.Errorf("Getbalance error %s", err)
+            }
+            ShouldEqual(bal, leftVal)
+            wg.Done()
+        }(j)
     }
     wg.Wait()
-    bal, err := wallet.GetBalance(node1, ipPort, accounts[0].addr)
+    gasBal, _ = wallet.GetBalance(node1, ipPort, gasAccount)
+    t.Log(gasAccount,gasBal)
+    bal, err := wallet.GetBalance(node1, ipPort, receiveAddr)
     if err != nil {
         t.Errorf("TestMultSendOne GetBalance error %s", err)
     }
-    if bal != "40000000000000000000" {
-        t.Errorf("test MultSendOne get balance error %s", err)
+    if bal != "30000000000000000000" {
+        t.Errorf("test MultSendOne get balance error %s", bal)
     }
 }
 
-//Test using one account send coins to multiple account at the same time
-//generate multiple accounts, init one of them with 100 ankr token
-// send 10 ankr token to others from the init account, check balance of the receive accounts and the init account
-func TestOneSendMult(t *testing.T) {
+// test using one account continuously send to other account
+func TestOneKeepSending(t *testing.T) {
     checkoutTestAccount()
-    Convey("test multiple account send to one account at the same time", t, func() {
+    Convey("test using one account continuously send to different account", t, func() {
+        base, _ := new(big.Int).SetString(balBase, 10)
+
+        initAmount := new(big.Int)
+        initAmount = initAmount.Mul(base, big.NewInt(110))
+        //init account
+        initPriv, _, initAddr := wallet.GenerateKeys()
+         _, err := wallet.SendCoins(node1, ipPort, adminPrivKey, adminAddress, initAddr, initAmount.String())
+         So(err, ShouldBeNil)
+
+        sendVal := new(big.Int)
+        sendVal = sendVal.Mul(base, big.NewInt(10))
         var accounts []account
-        val := "20000000000000000000" //20 ankr token
-        for i := 0; i < 20; i++ {
+        for i := 0; i < 10; i++ {
             priv, pub, addr := wallet.GenerateKeys()
             accounts = append(accounts, account{priv: priv, pub: pub, addr: addr})
-            _, err := wallet.SendCoins(node1, ipPort, adminPrivKey, adminAddress, addr, val)
+            _, err := wallet.SendCoins(node1, ipPort, initPriv, initAddr, addr, sendVal.String())
             ShouldBeNil(err)
         }
+        leftBal := initAmount.Mul(base, big.NewInt(5))
+        initBal, _ := wallet.GetBalance(node1, ipPort, initAddr)
+        So(initBal, ShouldEqual, leftBal.String())
+
+        receiveBal := initAmount.Mul(base, big.NewInt(5))
         for _, acc := range accounts {
-            balance, _ := wallet.GetBalance(node1, ipPort, acc.addr)
-            So(balance, ShouldEqual, "15000000000000000000")
+            bal, _ := wallet.GetBalance(node1, ipPort, acc.addr)
+            So(bal, ShouldEqual, receiveBal.String())
         }
-        sendVal := "10000000000000000000" //10 ankr token
-        for j := 1; j < 20; j++ {
-            _, err := wallet.SendCoins(node1, ipPort, accounts[j].priv, accounts[j].addr, accounts[0].addr, sendVal)
+    })
+
+    Convey("test using one account continuously sending to another account", t, func() {
+        base, _ := new(big.Int).SetString(balBase, 10)
+        initAmount := new(big.Int)
+        initAmount = initAmount.Mul(base, big.NewInt(110))
+        //init account
+        initPriv, _, initAddr := wallet.GenerateKeys()
+        _, err := wallet.SendCoins(node1, ipPort, adminPrivKey, adminAddress, initAddr, initAmount.String())
+        So(err, ShouldBeNil)
+        sendVal := new(big.Int)
+        sendVal = sendVal.Mul(base, big.NewInt(10))
+        _, _, receiveAddr := wallet.GenerateKeys()
+        for i := 0; i < 10 ; i ++ {
+            _, err := wallet.SendCoins(node1, ipPort, initPriv, initAddr, receiveAddr, sendVal.String())
             So(err, ShouldBeNil)
         }
-        balance, err := wallet.GetBalance(node1, ipPort, accounts[0].addr)
-        So(err, ShouldBeNil)
-        So(balance, ShouldEqual, "110000000000000000000")
+        checkVal := sendVal.Mul(base, big.NewInt(5))
+        initAccBal, _ := wallet.GetBalance(node1, ipPort, initAddr)
+        So(initAccBal, ShouldEqual, checkVal.String())
+
+        receiveVal := sendVal.Mul(base, big.NewInt(50))
+        receiveBal, _ := wallet.GetBalance(node1, ipPort, receiveAddr)
+        So(receiveBal, ShouldEqual, receiveVal.String())
     })
 }
 
-func TestMultSendOnePara(t *testing.T) {
-    checkoutTestAccount()
-    var accounts []account
-    for i := 0; i < 20; i++ {
-        priv, pub, addr := wallet.GenerateKeys()
-        accounts = append(accounts, account{priv: priv, pub: pub, addr: addr})
-        _, err := wallet.SendCoins(node1, ipPort, adminPrivKey, adminAddress, addr, sendVal)
-        if err != nil {
-            t.Errorf("send coin error %s", err)
-        }
-        balance, err := wallet.GetBalance(node1, ipPort, accounts[0].addr)
-        if err != nil {
-            t.Errorf("get balance error %s", err)
-        }
-        fmt.Println("get balance ", balance)
-    }
-    wg := sync.WaitGroup{}
-    for j := 1; j < 20; j++ {
-        wg.Add(1)
-        go func(j int, wgg *sync.WaitGroup) {
-            _, err := wallet.SendCoins(node1, ipPort, accounts[j].priv, accounts[j].addr, accounts[0].addr, sendVal2)
-            if err != nil {
-                t.Errorf("send coins error %s", err)
-            }
-            wgg.Done()
-        }(j, &wg)
-    }
-    wg.Wait()
-    balance, err := wallet.GetBalance(node1, ipPort, accounts[0].addr)
-    if err != nil {
-        t.Errorf("get balance error %s", err)
-    }
-    if balance == "110000000000000000000" {
-        fmt.Printf("%s", balance)
-    } else {
-        t.Errorf("get balance amount error %s ", err)
-    }
-}
-
+// todo
+// need another url
 func TestMultNodeSendOnePara(t *testing.T) {
     checkoutTestAccount()
     var accounts []account
@@ -172,6 +233,8 @@ func TestMultNodeSendOnePara(t *testing.T) {
     }
 }
 
+//todo
+// need another node url
 func TestOneDiffNodeSendMult(t *testing.T) {
     checkoutTestAccount()
     var accounts []account
--- integrationtesting/wallter_api_test.go
+++ integrationtesting/wallter_api_test.go
@@ -1,7 +1,6 @@
 package tester
 
 import (
-    "math"
     "math/big"
     "testing"
 
@@ -19,36 +18,10 @@ func TestGenerateKeys(t *testing.T) {
     })
 }
 
-func TestSetBalance(t *testing.T) {
-    checkoutTestAccount()
-    sendAmount := "100000000000000000000"
-    Convey("Test SetBalance main flow, should return nil", t, func() {
-        _, _, addr := wallet.GenerateKeys()
-        wallet.SendCoins(node1, ipPort, adminPrivKey, adminAddress, addr, sendAmount)
-        err := wallet.SetBalance(node1, ipPort, adminAddress, sendAmount, adminPrivKey)
-        ShouldBeNil(err)
-
-        //check SetBalance result
-        balance, err := wallet.GetBalance(node1, ipPort, addr)
-        So(err, ShouldBeNil)
-        So(balance, ShouldEqual, "95000000000000000000")
-    })
-
-    //test SetBalance when the value is set a neg number
-    Convey("TestSetBalance should fail if set balance a nge number", t, func() {
-        setVal := big.NewInt(-1)
-        _, _, addr := wallet.GenerateKeys()
-        err := wallet.SetBalance(node1, ipPort, addr, setVal.String(), adminPrivKey)
-        So(err, ShouldBeError)
-    })
-
-}
-
-//
 func TestSendCoin(t *testing.T) {
     checkoutTestAccount()
-    _, _, receiveAddr := wallet.GenerateKeys()
     Convey("Test SendCoin main flow, should return nil", t, func() {
+        _, _, receiveAddr := wallet.GenerateKeys()
         _, err := wallet.SendCoins(node1, ipPort, adminPrivKey, adminAddress, receiveAddr, sendVal2)
         So(err, ShouldBeNil)
         balanceT, err := wallet.GetBalance(node1, ipPort, receiveAddr)
@@ -56,24 +29,15 @@ func TestSendCoin(t *testing.T) {
         So(balanceT, ShouldEqual, "5000000000000000000")
     })
 
-    Convey("TestSendCoin should success if send more than max uint64", t, func() {
-        maxmum := new(big.Int).SetUint64(math.MaxUint64)
-        _, err := wallet.SendCoins(node1, ipPort, adminPrivKey, adminAddress, receiveAddr, maxmum.Add(maxmum, big.NewInt(1)).String())
-        So(err, ShouldBeNil)
-        balance1, err := wallet.GetBalance(node1, ipPort, receiveAddr)
-        So(err, ShouldBeNil)
-        So(balance1, ShouldEqual, "18446744073709551616")
-    })
-
     Convey("TestSendCoin should fail if send more than total supply", t, func() {
+        _, _, receiveAddr := wallet.GenerateKeys()
         totalSupply := new(big.Int)
         ankrBase := big.NewInt(1000000000000000000)
         totalSupply = totalSupply.Mul(ankrBase, big.NewInt(1000000000000000000))
         _, err := wallet.SendCoins(node1, ipPort, adminPrivKey, adminAddress, receiveAddr, totalSupply.Add(totalSupply, big.NewInt(1)).String())
         So(err, ShouldBeError)
-        balance1, err := wallet.GetBalance(node1, ipPort, receiveAddr)
-        So(err, ShouldBeNil)
-        So(balance1, ShouldEqual, "18446744073709551616")
+        _, err = wallet.GetBalance(node1, ipPort, receiveAddr)
+        So(err, ShouldBeError)
     })
 }
 
@@ -81,5 +45,13 @@ func TestGetMeter(t *testing.T) {
     Convey("test get meter", t, func() {
         _, err := wallet.GetHistoryMetering(node1, ipPort, "datacenter_name", "test-deploy", false, 0, 0)
         So(err, ShouldBeNil)
+    })
+}
+
+func TestGetBalance(t *testing.T)  {
+    Convey("test getbalance", t, func() {
+        bal, err := wallet.GetBalance(node1, ipPort, "64BC85F08C03F42B17EAAF5AFFAF9BFAF96CFCB85CA2F3")
+        ShouldNotBeNil(err)
+        t.Log("64BC85F08C03F42B17EAAF5AFFAF9BFAF96CFCB85CA2F3", bal)
     })
 }
