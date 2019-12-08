90e5910b55113ecbe72effaaca1a1769398e4123
likaioc <likai@ankr.com>
2019-12-09 01:09:55 +0800

    modify the unnecessary version infos for contract compiler

--- tool/compiler/root/version.go
+++ tool/compiler/root/version.go
@@ -12,7 +12,7 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print the contract compiler version",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Version:",version.CompilerVersion)
+		fmt.Println(version.CompilerVersion)
 	},
 }
 
