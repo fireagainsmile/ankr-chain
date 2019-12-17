25e9f31147530f7f13a624f88a9ae5c99c53b32d
likaioc <likai@ankr.com>
2019-12-17 15:46:33 +0800

    change cdc v0 msg's registered path

--- tx/cdcv0/codec.go
+++ tx/cdcv0/codec.go
@@ -10,8 +10,8 @@ func CreateTxCDC() *amino.Codec {
 	txCdc := amino.NewCodec()
 	cryptoAmino.RegisterAmino(txCdc)
 	txCdc.RegisterInterface((*tx.ImplTxMsgCDCV0)(nil), nil)
-	txCdc.RegisterConcrete(&tx.TxMsgCDCV0{}, "ankr-chain/tx/txMsgCDCV0", nil)
-	txCdc.RegisterConcrete(&TransferMsg{}, "ankr-chain/tx/cdcv0/tranferTxMsg", nil)
+	txCdc.RegisterConcrete(&tx.TxMsgCDCV0{}, "ankr-chain/tx/txMsg", nil)
+	txCdc.RegisterConcrete(&TransferMsg{}, "ankr-chain/tx/token/tranferTxMsg", nil)
 
 	return txCdc
 }
