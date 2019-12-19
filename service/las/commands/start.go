bf6994b6d7c7cb49e926d6c094ceb55d05681025
likaioc <likai@ankr.com>
2019-12-19 22:59:52 +0800

    adjust the las structure

--- service/las/commands/start.go
+++ service/las/commands/start.go
@@ -4,10 +4,8 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/Ankr-network/ankr-chain/client"
 	lascmm "github.com/Ankr-network/ankr-chain/service/las/common"
 	lashandler "github.com/Ankr-network/ankr-chain/service/las/handler"
-	"github.com/gorilla/mux"
 	"github.com/rakyll/statik/fs"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -23,7 +21,7 @@ func Start() *cobra.Command {
 		Example: "ankrchain-las start --chain-id=<chain-id> --proof-verify --node=<node connection address, such as tcp://127.0.0.1:26657, http://127.0.0.1:26657 and https://127.0.0.1:443>",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			listenAddr := viper.GetString(lascmm.FlagListenAddr)
-			router := registerHandler()
+			router := lashandler.RegisterHandler()
 
 			statikFS, err := fs.New()
 			if err != nil {
@@ -55,7 +53,6 @@ func Start() *cobra.Command {
 
 			logger.Info("ankrchain-las server started")
 
-			// wait forever and cleanup
 			cmn.TrapSignal(logger, func() {
 				err := listener.Close()
 				logger.Error("error closing listener", "err", err)
@@ -73,22 +70,4 @@ func Start() *cobra.Command {
 	cmd.Flags().Bool(lascmm.FlagProofVerify, false, "Needn't verify proofs of responses")
 
 	return cmd
-}
-
-func registerHandler() *mux.Router {
-	r := mux.NewRouter()
-
-	nodeAddr := viper.GetString(lascmm.FlagNode)
-
-	c := client.NewClient(nodeAddr)
-	r.HandleFunc("/v1/version", lashandler.QueryVersionHandler()).Methods("GET")
-	r.HandleFunc("/v1/node/info", lashandler.QueryNodeInfoHandler(c)).Methods("GET")
-	r.HandleFunc("/v1/account/generate/{count}", lashandler.GenerateAccounts(c)).Methods("GET")
-	r.HandleFunc("/v1/account/nonce/{address}", lashandler.QueryAccountNonceHandler(c)).Methods("GET")
-	r.HandleFunc("/v1/account/balance", lashandler.QueryAccountBalanceHandler(c)).Methods("GET")
-	r.HandleFunc("/v1/tx/transfer", lashandler.TxTransferHandler(c)).Methods("POST")
-	r.HandleFunc("/v1/block/tx/transfers", lashandler.QueryBlockTxTransfersHandler(c)).Methods("GET")
-	r.HandleFunc("/v1/block/syncing", lashandler.QueryBlockSyncing(c)).Methods("GET")
-
-	return r
 }
